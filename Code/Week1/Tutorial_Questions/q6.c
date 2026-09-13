8a16bd2827f773c999e57a7ddbec612c478ca5f0
Sophia Budkin <z5687506@ad.unsw.edu.au>
2026-09-13 23:35:25 +1000

    Reset questions to starter stage.

--- Code/Week1/Tutorial_Questions/q6.c
+++ Code/Week1/Tutorial_Questions/q6.c
@@ -2,13 +2,9 @@
 
 // Rewritten for loop version
 int main(void) {
-    // Since i is initialised inside the loop structure, once outside
-    // the for loop we cannot access the contents of it again!
-    for (int i = 0; i < 10; i++) {
-        printf("%d\n", i);
-    }
-
-    return 0;
+    // TO DO:
+    // rewrite the while loop in main_original(), using a for loop
+    // instead!
 }
 
 // Original while loop version
