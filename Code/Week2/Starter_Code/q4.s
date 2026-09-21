0e6ab39b9236272b7405cc56f3c7ae022f013c17
Sophia Budkin <z5687506@ad.unsw.edu.au>
2026-09-21 15:52:39 +1000

    class

--- Code/Week2/Starter_Code/q4.s
+++ Code/Week2/Starter_Code/q4.s
@@ -19,6 +19,31 @@
 
 # Hint: what's the first basic building block of a program we need to have in MIPS to get
 # it to run?
+main:
+	# reg allocations:
+	# int x - $t0
+	# int y  - $t1
+
+	li	$v0, 4
+	la	$a0, enter_num_str
+	syscall
+
+	li	$v0, 5
+	syscall
+	move	$t0, $v0
+
+	mul	$t1, $t0, $t0
+
+	li	$v0, 1
+	move	$a0, $t1
+	syscall
+
+	li	$v0, 11
+	li	$a0, '\n'
+	syscall
+
+	li	$v0, 0
+	jr	$ra
 
 	.data
 enter_num_str:
